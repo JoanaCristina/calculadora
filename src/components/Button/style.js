9c26029c761fa76fe895b6fc974e24f96ddeb4ec
JoanaCristina <joana.cristina_s@hotmail.com>
2024-09-07 16:33:35 -0300

    Calculadora em React JS

---
+++ src/components/Button/style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ButtonContainer= styled.button`
+padding:10px 20px;
+border: 1px solid #a2a49f;
+background-color: #00AAF0;
+color: #ffffff;
+margin: 1px;
+flex:1;
+font-size: 24px;
+font-weight: 700; 
+&:hover{
+opacity:0.6;
+}
+
+
+`
